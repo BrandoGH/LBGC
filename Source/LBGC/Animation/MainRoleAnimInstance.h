6393156cc3e7a70b8552d16c1f1be3c363973f42
brando <anonisboo@gmail.com>
2022-11-13 21:58:03 +0800

    add animation ik rig

---
+++ Source/LBGC/Animation/MainRoleAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MainRoleAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LBGC_API UMainRoleAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
